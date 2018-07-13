c0a20b17a82446602c589c39009046ab04a947d5
bcumming <bcumming@cscs.ch>
2018-07-13 17:14:35 +0200

    update diffusion plotting script

--- topics/cuda/practicals/diffusion/plotting.py
+++ topics/cuda/practicals/diffusion/plotting.py
@@ -3,9 +3,7 @@
 # CSCS - Summer School 2015
 # Written by Jean M. Favre
 #
-# Tested on daint, with python/2.7.6
-# Date: Thu May 21 14:11:38 CEST 2015
-#
+# Tested on daint, with python/2.7.13
 
 import pylab as pl
 import numpy as np
@@ -18,8 +16,7 @@ headerfile.close()
 
 rawFilename = header[1].split()[1]
 
-res_x = int(header[2].split()[1][:-1]) # remove the ',' at end of string
-res_y = int(header[2].split()[2][:-1]) # remove the ',' at end of string
+res_x, res_y = [int(x) for x in header[2].strip().split()[1:3]]
 print 'xdim %s' % res_x
 print 'ydim %s' % res_y
 
@@ -29,12 +26,10 @@ assert data.shape[0] == res_x * res_y, "raw data array does not match the resolu
 x = np.linspace(0., 1., res_x)
 y = np.linspace(0., 1.*res_y/res_x, res_y)
 X, Y = np.meshgrid(x, y)
-# number of contours we wish to see
-N = 12
-pl.contourf(X, Y, data.reshape(res_y, res_x), N, alpha=.75, cmap='jet')
-C=pl.contour(X, Y, data.reshape(res_y, res_x), N, colors='black', linewidth=.1)
+# contour intervals
+V = [-0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.01]
+C=pl.contourf(X, Y, data.reshape(res_y, res_x), V, alpha=.75, cmap='jet')
 
-#pl.clabel(C, inline=1)
 pl.axes().set_aspect('equal')
 pl.savefig("output.png", dpi=72)
 pl.show()
